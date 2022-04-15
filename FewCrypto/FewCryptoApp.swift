772796aaa6379904d2d21170875360c04bb59446
“Fewcher” <ffewcher@gmail.com>
2022-04-15 14:20:11 +0500

    Initial Commit

---
+++ FewCrypto/FewCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FewCryptoApp.swift
+//  FewCrypto
+//
+//  Created by Fewcher on 15.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FewCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
