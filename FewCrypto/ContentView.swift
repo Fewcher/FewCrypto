9badce3df172e45e04c7ad0a5bca0dd1d8cd0372
“Fewcher” <ffewcher@gmail.com>
2022-05-04 14:58:40 +0500

    fix before making computed property

--- FewCrypto/ContentView.swift
+++ FewCrypto/ContentView.swift
@@ -10,45 +10,31 @@ import WidgetKit // need this to refresh the widget by pressing the button
 
 struct ContentView: View {
     
-//    @State private var bitcoin = ""
-//    @State private var dollars = 0
-//    @State private var rub = 0
-//    @State private var dateStr = "never"
-    
-//    @AppStorage("bitcoin") private var bitcoin = "0"
-//    @AppStorage("usd") private var usd = 0
-//    @AppStorage("rub") private var rub = 0
-    // storing with UserDefaults
-    // попробуй заменить UserDefaults(suiteName: "group.FewCrypto.Fewcher") на @AppStorage
-        // когда ВСЕ переменные были @appstorage чот не работает
-    
         @AppStorage("bitcoin") private var bitcoin = "0"
-        //@State private var bitcoin = UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.string(forKey: "bitcoin") ?? "0.11"
-        //при загрузке формы вариант со @state userdefaults показывает опциональное значение
+//      @State private var bitcoin = UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.string(forKey: "bitcoin") ?? "0.11"
+//     при загрузке формы вариант со @state userdefaults показывает опциональное значение
     
-//        @State private var usd = UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.integer(forKey: "usd")
-//        @State private var rub = UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.integer(forKey: "rub")
-//        @State var date = UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.string(forKey: "date")
+//      @State private var usd = UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.integer(forKey: "usd")
+//      @State private var rub = UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.integer(forKey: "rub")
+//      @State private var date = UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.string(forKey: "date")
+//     не обновляет данные при таком объявлении
+//     еще бы убрал умножение на лету
     
-//     @AppStorage("dateStr") private var dateStr = "Updated: Never"
-//     @AppStorage("date") private var date = "Updated: Never"
-    //private var date = UserDefaults(suiteName: "group.FewCrypto_NM.Fewcher")!.string(forKey: "date")
-    //dateStr = "Updated: " + String(Date.now.formatted(date: .long, time: .shortened))
-    // date-time of last update
+//      dateStr = "Updated: " + String(Date.now.formatted(date: .long, time: .shortened))
+
+    let defaults = UserDefaults(suiteName: "group.FewCrypto.Fewcher") // экономия места
     
-    let rub = UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.integer(forKey: "rub")
-    let usd = UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.integer(forKey: "usd")
-    let date = UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.string(forKey: "date")
-    
-    @FocusState private var focus: Bool
+    @FocusState private var focus: Bool // чтобы убирать клаву
     
     @State private var isShowingField = false
-    @State private var btcAdd: String = "+++"
+    @State private var btc1: String = "first"
+    @State private var btc2: String = "second"
     
     @ObservedObject var networkManager = NetworkManager()
     
     var body: some View {
         Form {
+            let date = defaults!.string(forKey: "date")
             Text("Updated: " + (date ?? "never"))
                 .font(.footnote)
                 .foregroundColor(.gray)
@@ -56,7 +42,7 @@ struct ContentView: View {
             Section {
                 HStack {
                     Text("₿ ")
-                    TextField("0", text: $bitcoin)
+                    TextField("0", text: $bitcoin) // need $ (binding) to read AND write data and update UI
                         .keyboardType(.decimalPad)
                         .focused($focus)
                         .onChange(of: bitcoin){ newValue in // calculate value 'on the go'
@@ -74,58 +60,52 @@ struct ContentView: View {
                         isShowingField.toggle()
                     }
                 }
-                
-
-                
                 if isShowingField {
-                    //TextField("+++")
-                    TextField("?", text: $btcAdd)
-                        .transition(.slide) // gotta find good transition effect. maybe .opacity
+                    HStack {
+                        TextField("0", text: $btc1)
+                            //.transition(.slide) // gotta find good transition effect: maybe .opacity
+                            // BUT it does not seem to be working with Sections
+                        
+                        Divider()
+                        
+                        TextField("0", text: $btc2)
+                    }
+
                 }
             }
+            // MARK: upper field has to be computed property with bitcoin variable going to first lower field and
             
             Section {
                 HStack {
                     Text("$ ")
-                    //Text("\(usd)")
-//                usd = Int((Double(bitcoin) ?? 0) * Double(results.bitcoin.usd))
-//                //привожу к int чтобы убрать десятичные
-                    //Text(Int((Double(bitcoin) ?? 0) * Double(usd)))
-
-                    let usdBitcoin = Int((Double(bitcoin) ?? 0) * Double(usd))
-                    Text("\(usdBitcoin)")
-                    // мне не нравится как это выглядит, но хз чо еще придумать
-                    // мне не нравится что умножение на лету, но я хз как убрать
-                }
                 
-                Text("1 BTC = \(usd) usd")
-                    .foregroundColor(.secondary)
-                    .font(.caption)
-                    .opacity(0.5)
+                    let usd = defaults!.integer(forKey: "usd")
+                    let usdBitcoin = Int((Double(bitcoin) ?? 0) * Double(usd))
+                    // привожу к int чтобы убрать десятичные
+                    Text("\(usdBitcoin)")
+                }
+            } footer: {
+                let usd = defaults!.integer(forKey: "usd")
+                Text("1₿ = $\(usd) ")
             }
             
             Section {
                 HStack {
                     Text("₽ ")
-            
+                    let rub = defaults!.integer(forKey: "rub")
                     let rubBitcoin = Int((Double(bitcoin) ?? 0) * Double(rub))
                     Text("\(rubBitcoin)")
-                    // rub = Int((Double(bitcoin) ?? 0) * Double(results.bitcoin.rub))
                 }
-                
-                Text("1 BTC = \(rub) rub")
-                    .foregroundColor(.secondary)
-                    .font(.caption)
-                    .opacity(0.5)
+            } footer: {
+                let rub = defaults!.integer(forKey: "rub")
+                Text("1₿ = \(rub) ₽")
             }
-            
-
             
             Button("Update") {
                 //self.networkManager.fetchData()
                 networkManager.fetchData { usd in } // хз почему это работает
                 
-                UserDefaults(suiteName: "group.FewCrypto.Fewcher")!.set(bitcoin, forKey: "bitcoin")
+                defaults!.set(bitcoin, forKey: "bitcoin")
                 print(bitcoin)
                 
                 WidgetCenter.shared.reloadAllTimelines() // reload all widgets
